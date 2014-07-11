8d8af0294d86c128042b25b53ab551a7f5b5dbbe
Guillaume Destuynder <kang@mozilla.com>
2014-07-11 13:19:31 -0700

    Add MPL header

--- nfldap_reload.py
+++ nfldap_reload.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Contributors:
+# jdow@mozilla.com
+# jvehent@mozilla.com
+# bhourigan@mozilla.com
+# gdestuynder@mozilla.com
+#
 # Requires:
 # libnfldap
 
